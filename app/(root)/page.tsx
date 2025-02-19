995e6eaed16a431aa121c000c2b8c92851c0f2db
ami-inn <amipk2001@gmail.com>
2025-02-19 11:51:04 +0530

    book overview section is completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,13 +3,21 @@
 import BookList from '@/components/BookList'
 import BookOverview from '@/components/BookOverview'
 import { Button } from '@/components/ui/button'
+import { sampleBooks } from '@/constants'
 import React from 'react'
 
 const Home = () => {
   return (
     <>
-      <BookOverview/>
-      <BookList/>
+      <BookOverview
+      {...sampleBooks[0]}
+      />
+      <BookList
+      title='Latest Books'
+      books={sampleBooks}
+      containerClassName='mt-28'
+      
+      />
     </>
   )
 }
---
+++ types.d.ts
@@ -0,0 +1,17 @@
+
+interface Book {
+    id:number;
+    title : string;
+    author : string;
+    genre : string;
+    rating : number;
+    total_copies : number;
+    available_copies : number;
+    description:string;
+    color : string;
+    coverUrl : string;
+    video : string;
+    summary : string;
+    isLoadBook? : boolean;
+
+}
