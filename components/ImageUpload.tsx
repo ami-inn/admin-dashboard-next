864ffd705580eb18be262b9a2f044fc2a30d6e24
ami-inn <amipk2001@gmail.com>
2025-02-20 10:36:38 +0530

    imgae upload component updated with image kti

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,4 +1,33 @@
+
+'use client'
 import React from 'react'
+import { IKImage, ImageKitProvider, IKUpload } from "imagekitio-next";
+import config from '@/lib/config';
+
+const authenticator = async ()=>{
+    try {
+        const response = await fetch(`${config.env.apiEndPoint}/api/auth/imageKit`)
+      
+        if(!response.ok){
+          const errorText = await response.text()
+          throw new Error(`Request failed with status ${response.status}: ${errorText}`)
+        }
+
+    const data = await response.json()
+
+    const {signature,expires,token} = data
+    
+    return {signature,expires,token}
+
+        
+    } catch (error:any) {
+        throw new Error(`Error authenticating user: ${error}`)
+    }
+
+
+
+
+}
 
 const ImageUpload = () => {
   return (
---
+++ lib/config.ts
@@ -0,0 +1,17 @@
+
+
+const config = {
+    env:{
+
+        apiEndPoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
+
+        imageKit:{
+            publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
+            privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
+            urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL!
+        }
+    }
+}
+
+
+export default config;
