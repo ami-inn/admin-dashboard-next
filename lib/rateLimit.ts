b7d544b021970b4d9882f9024ae1b8a6a6f8f25c
ami-inn <amipk2001@gmail.com>
2025-02-21 22:21:20 +0530

    updated the rate limit

--- lib/rateLimit.ts
+++ lib/rateLimit.ts
@@ -3,10 +3,40 @@ import { Ratelimit } from "@upstash/ratelimit";
 
 const ratelimit = new Ratelimit({
   redis,
-  limiter: Ratelimit.fixedWindow(1,'1m'),
+  limiter: Ratelimit.fixedWindow(5,'1m'),
   analytics: true,
   prefix: "@upstash/ratelimit",
 });
+
+
+
+// upstash ratellimit github for the normal api path
+
+// export default async function middleware(
+//     request: NextRequest,
+//     context: NextFetchEvent,
+//   ): Promise<Response | undefined> {
+//     const ip = request.ip ?? "127.0.0.1";
+  
+//     const { success, pending, limit, remaining } = await ratelimit.limit(ip);
+//     // we use context.waitUntil since analytics: true.
+//     // see https://upstash.com/docs/oss/sdks/ts/ratelimit/gettingstarted#serverless-environments
+//     context.waitUntil(pending);
+  
+//     const res = success
+//       ? NextResponse.next()
+//       : NextResponse.redirect(new URL("/api/blocked", request.url));
+  
+//       res.headers.set("X-RateLimit-Success", success.toString());
+//       res.headers.set("X-RateLimit-Limit", limit.toString());
+//       res.headers.set("X-RateLimit-Remaining", remaining.toString());
+  
+//     return res;
+//   }
+  
+//   export const config = {
+//     matcher: "/api",
+//   };
 
 
 export default ratelimit;
